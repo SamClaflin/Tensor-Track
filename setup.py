41bd735ef64b910951b12f957f93dd67b939e408
Sam Claflin <samclaflin7@gmail.com>
2020-07-15 18:42:22 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="tensor-track",
+    version="0.0.1",
+    description="The one-stop shop for tracking TensorFlow metrics",
+    url="",
+    author="Sam Claflin",
+    author_email="samclaflin7@gmail.com",
+    license="MIT",
+    packages=["tensor-track"],
+    zip_safe=False
+)
