b8add4b56022bba7ec88dac14741c7c5962d72fe
Sam Claflin <samclaflin7@gmail.com>
2020-07-16 14:07:29 -0400

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="tensor_track",
     version="0.0.1",
     description="The one-stop shop for tracking TensorFlow metrics",
+    long_description=long_description,
+    long_desctiption_contant_type="text/markdown",
     url="https://github.com/SamClaflin/Tensor-Track",
     author="Sam Claflin",
     author_email="samclaflin7@gmail.com",
