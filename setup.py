7857bf049b49586f83303feffbcf9aea03589664
Sam Claflin <samclaflin7@gmail.com>
2020-07-16 15:17:24 -0400

    Removed long description

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 from setuptools import setup
 
-with open("README.md", "r") as f:
-    long_description = f.read()
-
 setup(
     name="tensor-track",
     version="0.0.1",
     description="The one-stop shop for tracking TensorFlow metrics",
-    long_description=long_description,
-    long_desctiption_content_type="text/markdown",
     url="https://github.com/SamClaflin/Tensor-Track",
     author="Sam Claflin",
     author_email="samclaflin7@gmail.com",
